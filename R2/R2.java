23e6f064dc4c0e882260230858c5efb865105b06
MathiasFaber <makr20ah@student.cbs.dk>
2023-09-20 11:15:50 +0200

    test ruleset

---
+++ R2/R2.java
@@ -0,0 +1,15 @@
+package R2;
+
+import java.util.Scanner;
+
+public class R2 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = b * 2 - a;
+        System.out.println(c);
+        // test ruleset
+    }
+}
