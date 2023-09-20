b0cffb4498a5f8a649d072ac4c873f68a15879b1
MathiasFaber <makr20ah@student.cbs.dk>
2023-09-20 10:07:39 +0200

    hej

--- Avogadro/Avogadro.java
+++ Avogadro/Avogadro.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Avogadro { // works but bad runtime
+public class Avogadro { // works but bad runtime, hej
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int columns = sc.nextInt();
